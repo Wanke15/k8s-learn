63cd0273a21be845aacc2f378d319c295b00a716
Jeff Wang <1015005300@qq.com>
2020-08-18 21:47:10 +0800

    change api blueprint

--- api/api.py
+++ api/api.py
@@ -1,7 +1,10 @@
 from flask import Blueprint
 
-from api.utils import logger
+import socket
 
+import hashlib
+
+from api.utils import logger
 
 base_bp = Blueprint("base", __name__)
 
@@ -10,3 +13,18 @@ base_bp = Blueprint("base", __name__)
 def health():
     logger.info("Service in good health!")
     return "I'am doing great!"
+
+
+colors = ["black", "blue", "green", "purple", "red", "pink", "orange"]
+
+
+def ab_bucket(data):
+    bucket_int = int(hashlib.md5(data.encode('utf8')).hexdigest(), 16) % len(colors)
+    return bucket_int
+
+
+@base_bp.route('/')
+def hello_world():
+    host_name = socket.gethostname()
+    return 'Hello world! This is served by host: <font color="{}">{}</font>'.format(colors[ab_bucket(host_name)],
+                                                                                    host_name)
